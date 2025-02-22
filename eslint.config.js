874baa90c3f4a6b4aaa32d08863415f991b27444
Stefan Kamphuis <102509739+PointlessPancake@users.noreply.github.com>
2025-02-22 18:35:13 +0100

    chore(lint): ignore PnP files in ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.pnp.cjs', '.pnp.loader.mjs'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
